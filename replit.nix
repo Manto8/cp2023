796b5c9fb56a700e460101fad1b5e9fe912f144b
Manto8 <144768391+Manto8@users.noreply.github.com>
2023-12-12 07:44:48 +0000

    新增W13頁面

--- replit.nix
+++ replit.nix
@@ -1,7 +1,18 @@
 { pkgs }: {
     deps = [
+      pkgs.libev
+      pkgs.pandoc
+      pkgs.glibcLocales
+      pkgs.gitFull
       pkgs.gnuplot
       pkgs.ncurses.dev
       pkgs.gd
     ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libev
+      pkgs.glibcLocales
+      pkgs.gitFull
+    ];
+  };
 }
